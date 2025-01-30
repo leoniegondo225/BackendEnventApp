fd6d23f23488fe43b201d78c580cad20abbb83be
leonie <leoniegondo@gmail.com>
2025-01-30 11:22:46 +0000

    Ajout de la securité cors

--- index.mjs
+++ index.mjs
@@ -1,12 +1,29 @@
 import express from "express"
 import route from "./routes/routes.mjs"
 import dotenv from "dotenv"
-import { MongooseConnect } from "./db/mongoose.mjs"
+import cors from "cors"
 dotenv.config()
 
 const app = express()
 
+//Pour donner l'autorisation à des domaines sur votre serveur
+const domaineAutorise = ["http://localhost:5173", "https://frontend-login-rho.vercel.app/"]
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || domaineAutorise.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error("Domaine non autorisé par le cors"))
+        }
+    }
+}
+app.use(cors(corsOptions))
+
+
+
 app.use(express.json())
+
 app.use("/api", route)
 
 app.get("/", (req, res) => res.json("Bienvenue sur notre api"))
